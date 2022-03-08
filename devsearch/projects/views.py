e2a6abb6139ff6ec1a994e4da77e857b01c8c2c1
jun20 <aljunma20@gmail.com>
2022-03-07 22:39:53 -1000

    adding db and urls

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website'
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work'
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community'
+    }
+]
+
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    projects = Project.objects.all()
+    context = { 'projects': projects }
+    #return HttpResponse('Here are our projects')
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    return HttpResponse('Single project')
+    #return HttpResponse('Single project')
+    # projectObg = None
+    # for i in projectsList:
+    #     if i['id'] == pk:
+    #         projectObg = i
+
+    projectObj = Project.objects.get(id=pk)   
+    tags = projectObj.tags.all()         
+    return render(request, 'projects/single-project.html', {'project': projectObj,'tags': tags})
